e7d59278ad7fea618dad5affad860a5a7d985ff4
Ahmed <a.hagagabdallah@gmail.com>
2023-05-10 00:31:49 +0300

    First commit w/ login and signup

---
+++ accounts/forms.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "age",
+        )
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "age",
+        )
