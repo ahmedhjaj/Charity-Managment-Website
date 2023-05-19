845bde0fc32a0844f3d7ea28703ddb5c45427f07
Ahmed <a.hagagabdallah@gmail.com>
2023-05-20 01:13:10 +0300

    Hosting 1

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-from environs import Env  # new
+from environs import Env 
 
-
+env = Env()
+env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
